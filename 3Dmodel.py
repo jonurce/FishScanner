86e25da415ced7c070ccb2fd65045ca174ef0d76
jonurce <jonur@stud.ntnu.no>
2025-02-03 16:17:21 +0100

    Test

--- 3Dmodel.py
+++ 3Dmodel.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-# Set paths to COLMAP executable and your images HEY
+# Set paths to COLMAP executable and your images
 colmap_path = '/path/to/colmap'  # Path to the COLMAP binary (replace with actual path)
 image_dir = 'Pictures/SmallPlastic'  # Path to the directory containing the 2D images
 output_dir = 'output_3d_model'  # Directory to store the 3D model
